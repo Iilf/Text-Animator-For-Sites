d5d56527e284f09cfd15076c81082e78ef481a20
Iilf <14dziegielewskiszy@ab-ed.org>
2025-12-04 03:52:37 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './app.tsx'
 
 // This code connects your React App component to the main index.html file's 'root' element.
 ReactDOM.createRoot(document.getElementById('root')!).render(
